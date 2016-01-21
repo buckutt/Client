d1aa15ba2d0125ab5a8fbc2e95d553f824f066d8
Gabriel Juchault <gabriel.juchault@gmail.com>
2016-01-21 14:15:30 +0100

    Cleaner article points filter

--- src/app/articles/filterPoint.js
+++ src/app/articles/filterPoint.js
@@ -1,13 +1,3 @@
-const filterPointArticle = (article, pointId) => {
-    let articleHasPoint = article.points.filterObjId(pointId) !== undefined;
-
-    if (!articleHasPoint) {
-        return null;
-    }
-
-    return article;
-};
-
 export default {
     methods: {
         /**
@@ -16,7 +6,7 @@ export default {
         filterPoint() {
             console.info('Filtering articles', this.articles.length);
 
-            this.articles = this.articles.map(article => filterPointArticle(article, this.pointId));
+            this.articles = this.articles.filter(article => article.points.filterObjId(this.pointId))
         }
     }
 }
