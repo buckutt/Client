3b131967f52c5bb44da11290bfbaef2b2ca6dca4
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2016-11-21 20:37:03 +0100

    fix: hotfix default server

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,4 @@
 module.exports = {
-  API: `"https://${(process.env.API || 'server').trim()}:3000"`,
+  API: `"https://${(process.env.API || 'localhost').trim()}:3000"`,
   NODE_ENV: '"production"'
 }
