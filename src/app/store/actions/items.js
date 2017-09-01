a7e2bab8dc7378b9cdbfb0f359e25ec123ec7fb1
Lonk <rinke2005@gmail.com>
2017-09-01 18:25:51 +0200

    fix: resize images to make it fit inside the item box

--- src/app/store/actions/items.js
+++ src/app/store/actions/items.js
@@ -4,6 +4,6 @@ export const getImage = (store, id) => {
     const token = store.getters.tokenHeaders;
 
     return axios
-        .get(`${config.images}/image/${id}`, token)
+        .get(`${config.images}/image/${id}?width=100&height=100`, token)
         .then(res => res.data.image);
 };
