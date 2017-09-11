776c9e6fd68dcea069a396f7e79f465ff9c40473
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2017-09-11 17:17:04 +0200

    fix: reload-only interface

--- src/app/store/actions/reload.js
+++ src/app/store/actions/reload.js
@@ -21,34 +21,8 @@ export const changeMeanOfPayment = ({ commit }, meanOfPayment) => {
     commit('CHANGE_MEAN_OF_PAYMENT', meanOfPayment);
 };
 
-export const addReload = (store, reload) => {
-    if (store.state.auth.seller.canReload && !store.state.auth.seller.canSell) {
-        const basketToSend = [
-            {
-                credit   : reload.amount,
-                trace    : reload.trace,
-                Buyer_id : store.state.auth.buyer.id,
-                Seller_id: store.state.auth.seller.id,
-                type     : reload.type
-            }
-        ];
-
-        axios
-            .post(`${config.api}/services/basket`, basketToSend, store.getters.tokenHeaders)
-            .then(() => {
-                store.commit('REMOVE_RELOADS');
-                store.commit('SET_LAST_USER', {
-                    name  : `${store.state.auth.buyer.firstname} ${store.state.auth.buyer.lastname}`,
-                    credit: store.state.auth.buyer.credit + reload.amount,
-                    reload: reload.amount,
-                    bought: 0
-                });
-
-                store.commit('LOGOUT_BUYER');
-            });
-    } else {
-        store.commit('ADD_RELOAD', reload);
-    }
+export const addReload = ({ commit }, reload) => {
+    commit('ADD_RELOAD', reload);
 };
 
 export const removeReloads = ({ commit }) => {
